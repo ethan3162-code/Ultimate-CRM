0eede5e30845bff40d124ca29a28a9c2d3702c6a
Eidan Levi <ethan3162@gmail.com>
2026-09-20 10:32:47 -0400

    Add reopenEstimate function to api.js

--- client/src/api.js
+++ client/src/api.js
@@ -125,6 +125,7 @@ export const api = {
   rejectEstimate: (estimateId, data) => request(`/jobs/estimates/${estimateId}/reject`, { method: 'POST', body: JSON.stringify(data || {}) }),
   sendEstimate: (estimateId, method) => request(`/jobs/estimates/${estimateId}/send`, { method: 'POST', body: JSON.stringify({ method }) }),
   signEstimateInPerson: (estimateId, data) => request(`/jobs/estimates/${estimateId}/sign`, { method: 'POST', body: JSON.stringify(data) }),
+  reopenEstimate: (estimateId) => request(`/jobs/estimates/${estimateId}/reopen`, { method: 'POST' }),
   updateEstimate: (estimateId, data) => request(`/jobs/estimates/${estimateId}`, { method: 'PATCH', body: JSON.stringify(data) }),
   duplicateEstimate: (estimateId) => request(`/jobs/estimates/${estimateId}/duplicate`, { method: 'POST' }),
   deleteEstimate: (estimateId) => request(`/jobs/estimates/${estimateId}`, { method: 'DELETE' }),
